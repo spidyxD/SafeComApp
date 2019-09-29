b37fd99dfbccb22677e6ba70fd1a81717b017aa2
andrsGutirrz <andres.gutierrez.arcia@gmail.com>
2019-09-28 22:20:49 -0600

    vista de lista de personas listo

--- frontend_django/SafeComApp/SafeComAppFrontend/urls.py
+++ frontend_django/SafeComApp/SafeComAppFrontend/urls.py
@@ -3,8 +3,13 @@ from . import views
 
 
 urlpatterns = [
-    #####################################
-    # INDEX/HOME
+    ##########################################################
+    # PERSONA URLS
     path('', views.home, name='index'),
-    path('registrarPersona', views.registrar_persona, name="registroPersona")
+    path('registrarPersona', views.registrar_persona, name="registroPersona"),
+    path('listarPersona', views.listar_persona, name="listarPersona"),
+    ##########################################################
+    # VEHICLE URLS
+    ##########################################################
+    # VISIT URLS
 ]
--- frontend_django/SafeComApp/SafeComAppFrontend/views.py
+++ frontend_django/SafeComApp/SafeComAppFrontend/views.py
@@ -5,17 +5,32 @@ from django.urls import reverse_lazy
 from django.http import Http404
 import json
 import datetime
-
+import requests
 
 # Create your views here.
 
 
 def home(request):
     """TO HOME PAGE"""
-    print("Hola")
-    return render(request, "SafeComAppFrontend/index.html", {})
+    # With context, we can send variables to templates
+    context = {'title': 'Inicio'}
+    return render(request, "SafeComAppFrontend/index.html", context)
+
 
 def registrar_persona(request):
     """REGISTRAR PERSONA"""
-    print("Registrar Persona")
-    return render(request, "SafeComAppFrontend/registrarPersona.html")
+    context = {'title': 'Registro Persona'}
+    return render(request, "SafeComAppFrontend/registrarPersona.html", context)
+
+
+def listar_persona(request):
+    """REGISTRAR PERSONA"""
+    response = requests.get('http://localhost:8000/api/persons/')
+    persons = response.json() #returns a list of dictionaries
+
+    context = {
+        'title': 'Personas',
+        'persons': persons,
+    }
+
+    return render(request, "SafeComAppFrontend/listaPersonas.html", context)
